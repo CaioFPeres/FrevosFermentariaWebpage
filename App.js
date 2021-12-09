c16cf2ddc427f42fda4908e8caf140ae305c5b98
Caio Peres <caiofperes@hotmail.com>
2021-12-09 13:47:52 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 (function(){
 
     const config = {
-      apiKey: "AIzaSyCnnDLOjq95sI2Nnu6miefytUxVWhieOtw",
-      authDomain: "frevos-fermentaria.firebaseapp.com",
-      databaseURL: "https://frevos-fermentaria.firebaseio.com",
-      projectId: "frevos-fermentaria",
-      storageBucket: "frevos-fermentaria.appspot.com",
-      messagingSenderId: "1064994762820",
-      appId: "1:1064994762820:web:d9dc9290f421bb59f18c11"
+        //firebase config goes here
     };
       firebase.initializeApp(config);
       
